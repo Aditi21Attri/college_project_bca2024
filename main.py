9696386933086e0fd4649210fb6ab922bf6426d3
Aditi21Attri <aditiattri07082002@gmail.com>
2024-01-26 12:49:17 +0530

    Important

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, session,redirect
+from flask import Flask, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
 import os
 from werkzeug.utils import secure_filename
+import time
 
 
 with open('templates/config.json', 'r') as file:
@@ -12,7 +13,7 @@ with open('templates/config.json', 'r') as file:
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 
-app.config["Uploader_url"]=params["file_save_url"]
+app.config["Uploader_url"] = params["file_save_url"]
 
 DATABSE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', password='',
                                                                                     server='localhost',
@@ -39,8 +40,9 @@ class hotelsdetails(db.Model):
     type = db.Column(db.String(20), nullable=False)
     hotel_slugs = db.Column(db.String(20), nullable=True)
     type_of = db.Column(db.String(20), nullable=True)
-    image_src=db.Column(db.String(20), nullable=True)
-    description=db.Column(db.String(20), nullable=False)
+    image_src = db.Column(db.String(20), nullable=True)
+    description = db.Column(db.String(20), nullable=False)
+
 
 class homestayvillas(db.Model):
     srno = db.Column(db.Integer, primary_key=True)
@@ -51,6 +53,12 @@ class homestayvillas(db.Model):
     description = db.Column(db.String(50), nullable=True)
     roomsoffered = db.Column(db.String(20), nullable=True)
     helpers = db.Column(db.String(50), nullable=True)
+
+
+@app.route("/hotelbooking/<string:srno>")
+def hotelbooking(srno):
+    hotel = hotelsdetails.query.filter_by(srno=srno).first()
+    return render_template("hotelbooking.html", hotel=hotel)
 
 
 @app.route("/register.html", methods={'GET', 'POST'})
@@ -81,13 +89,15 @@ def hotel():
     print(hotels)
 
     return render_template("hotel.html", hotels=hotels)
+
+
 @app.route("/homestay", methods={'GET', 'POST'})
 def homestayVillas():
     if request.method == "POST":
         destination = request.form.get('destination')
         helper_wanted = request.form.get('helper_wanted')
-        checkin=request.form.get('checkin')
-        checkout=request.form.get('checkout')
+        checkin = request.form.get('checkin')
+        checkout = request.form.get('checkout')
         range = request.form.get('range')
         homestays = homestayvillas.query.filter_by(area=destination)
     else:
@@ -95,6 +105,7 @@ def homestayVillas():
     print(homestays)
 
     return render_template("homestays.html", homestays=homestays)
+
 
 @app.route("/cabbooking", methods={'GET', 'POST'})
 def cabBooking():
@@ -109,9 +120,12 @@ def cabBooking():
     print(hotels)
 
     return render_template("cabbook.html", hotels=hotels)
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+
+    return render_template("index.html", )
 
 
 @app.route("/about")
@@ -127,14 +141,13 @@ def blog():
 @app.route("/services")
 def product():
     hotels = hotelsdetails.query.filter()[0:3]
-    homestays=homestayvillas.query.filter()[0:3]
+    homestays = homestayvillas.query.filter()[0:3]
     print(hotels)
-    return render_template("services.html",hotels=hotels,homestays=homestays)
+    return render_template("services.html", hotels=hotels, homestays=homestays)
+
 
 @app.route("/contact")
 def contact():
-
-
     return render_template("contact.html")
 
 
@@ -153,17 +166,17 @@ def login():
                 hotels = hotelsdetails.query.filter()
                 details_given = 'True'
                 return render_template("adminlogin.html", details_wanted=hotels, params=params,
-                                       details_given=details_given,details_type="hotels")
+                                       details_given=details_given, details_type="hotels")
             elif details_of == "homestayvillas":
                 homestays = homestayvillas.query.filter()
                 details_given = 'True'
                 return render_template("adminlogin.html", details_wanted=homestays, params=params,
-                                       details_given=details_given,details_type="homestays")
+                                       details_given=details_given, details_type="homestays")
             elif details_of == "signindetails":
                 users = signindetails.query.filter()
                 details_given = 'True'
                 return render_template("adminlogin.html", details_wanted=users, params=params,
-                                       details_given=details_given,details_type="users")
+                                       details_given=details_given, details_type="users")
 
             else:
                 return render_template("login.html")
@@ -192,7 +205,8 @@ def login():
     else:
         return render_template("login.html")
 
-@app.route("/uploadhotels/<string:srno>",methods=['GET','POST'])
+
+@app.route("/uploadhotels/<string:srno>", methods=['GET', 'POST'])
 def hoteldetails(srno):
     if 'user' in session and session['user'] == params["admin_user"]:
         if request.method == "POST":
@@ -201,39 +215,38 @@ def hoteldetails(srno):
             harea = request.form.get('location')
             cost = request.form.get('price')
             hotel_type = request.form.get('h_type')
-            f=request.files['file_path']
-            f.save(os.path.join(app.config["Uploader_url"],hname+(f.filename)))
-            image_src=hname+(f.filename)
+            f = request.files['file_path']
+            f.save(os.path.join(app.config["Uploader_url"], hname + (f.filename)))
+            image_src = hname + (f.filename)
             if srno == "0":
-                hotel_slug = "hotel_"+hname
+                hotel_slug = "hotel_" + hname
 
-                hotel_d = hotelsdetails(hotelname=hname, area=harea, price=cost, type=hotel_type,hotel_slugs=hotel_slug,image_src=image_src)
+                hotel_d = hotelsdetails(hotelname=hname, area=harea, price=cost, type=hotel_type,
+                                        hotel_slugs=hotel_slug, image_src=image_src)
                 db.session.add(hotel_d)
                 db.session.commit()
             else:
-                hotel=hotelsdetails.query.filter_by(srno=srno).first()
-                hotel.hotelname=hname
-                hotel.area=harea
-                hotel.price=cost
-                hotel.type=hotel_type
-                hotel.image_src=image_src
-                hotel.hotel_slugs="hotel_"+hname
+                hotel = hotelsdetails.query.filter_by(srno=srno).first()
+                hotel.hotelname = hname
+                hotel.area = harea
+                hotel.price = cost
+                hotel.type = hotel_type
+                hotel.image_src = image_src
+                hotel.hotel_slugs = "hotel_" + hname
                 db.session.commit()
-                return redirect("/uploadhotels/"+srno)
-        hotels=hotelsdetails.query.filter_by(srno=srno).first()
+                return redirect("/uploadhotels/" + srno)
+        hotels = hotelsdetails.query.filter_by(srno=srno).first()
 
         return render_template("uploadhotels.html", hotel=hotels)
 
-@app.route("/delete/<string:srno>",methods=['GET','POST'])
+
+@app.route("/delete/<string:srno>", methods=['GET', 'POST'])
 def delete_hotel(srno):
     if 'user' in session and session['user'] == params["admin_user"]:
         hotel = hotelsdetails.query.filter_by(srno=srno).first()
         db.session.delete(hotel)
         db.session.commit()
     return redirect("/login")
-
-
-
 
 
 app.run(debug=True)
