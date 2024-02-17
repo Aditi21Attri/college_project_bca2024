b30f6d6485d75cfd96ac3f268e3e102f3d80d5ad
Aditi21Attri <aditiattri07082002@gmail.com>
2024-02-17 17:36:37 +0530

    newadds

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
